2f712892e9da3682b0b1270554c083aeb8c3dfa2
Paul-Mad <paulin.mad@hotmail.com>
2021-03-24 11:57:48 +0000

    Updates

---
+++ precache-manifest.f27854b91b2c7d695cb8fb455778389e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f5893ab5d19245ec86e4f9eadec2094",
+    "url": "/robots/index.html"
+  },
+  {
+    "revision": "c1066eac42b8eef9f420",
+    "url": "/robots/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "567b59416a34eb3e71b4",
+    "url": "/robots/static/css/main.ca56f26a.chunk.css"
+  },
+  {
+    "revision": "c1066eac42b8eef9f420",
+    "url": "/robots/static/js/2.42b9c6b8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robots/static/js/2.42b9c6b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "567b59416a34eb3e71b4",
+    "url": "/robots/static/js/main.5e50e06e.chunk.js"
+  },
+  {
+    "revision": "df9e66469025a98d5aad",
+    "url": "/robots/static/js/runtime-main.e1a61dd3.js"
+  }
+]);
